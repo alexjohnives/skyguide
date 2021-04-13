84c35d45450fc129cdbad00e799dbf0f3286283d
alexjohnives <alexanderjohnives@gmail.com>
2021-04-13 14:37:40 -0400

    Saved as backup

---
+++ assets/js/scriptOne.js
@@ -0,0 +1,32 @@
+// This script ensures that as the webpage is loaded the addImageSelector function is applied. 
+
+window.addEventListener('load', (event) => {
+  
+    addImageSelector();
+  
+  });
+
+// This function targets the images with property img-select */
+
+  function addImageSelector() {
+    const imgSelected = document.querySelectorAll('.img-select');
+
+// When the image is selected on click, this click event toggles the 'active-img' class.
+  
+  imgSelected.forEach((imgSelect) => {
+    imgSelect.addEventListener('click', () => {
+      imgSelect.classList.toggle('activeimg');
+    });
+  });
+  }
+
+// This function displays the corresponding div with each image. 
+
+  function selectImage(elementId) {
+    var x = document.getElementById(elementId);
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  };
