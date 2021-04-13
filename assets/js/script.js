6ec66206c0a2116e23ccd1933c69f9f00d8f1cca
alexjohnives <alexanderjohnives@gmail.com>
2021-04-12 21:08:57 -0400

    Added Javascript and comments for image select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
+// This script ensures only once the webpage is fully loaded is the addImageSelector function applied 
+
 window.addEventListener('load', (event) => {
   
     addImageSelector();
   
   });
-    
+
+// This function targets the images with property img-select
+
   function addImageSelector() {
     const imgSelected = document.querySelectorAll('.img-select');
+
+// When the image is selected on click, this click event toggles the 'activeimg' class.
   
   imgSelected.forEach((imgSelect) => {
     imgSelect.addEventListener('click', () => {
