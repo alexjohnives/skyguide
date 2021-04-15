ce4dc97dcbc498f3b3d224d47f41c2d3f9fa7c8b
alexjohnives <alexanderjohnives@gmail.com>
2021-04-15 18:58:54 -0400

    Using email JS created working backend

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,21 @@
-// Created contact form and followed CI Emailjs lesson for javascript 
-function sendMail(contactForm) {
-  emailjs
-    .send('gmail', 'thatskyguide', {
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      message: contactForm.message.value,
-    })
-    .then(
-      function (response) {
-        console.log('SUCCESS', response);
-      },
-      function (error) {
-        console.log('FAILED', error);
-      }
-    );
-  return false; // To block from loading a new page
-}
+const btn = document.getElementById('button');
+
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  btn.value = 'Sending...';
+
+  const serviceID = 'default_service';
+  const templateID = 'skyguide contact';
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    },
+    (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    }
+  );
+});
