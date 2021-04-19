fd27d3945e875a46d1689d2fac21de14d96a044b
alexjohnives <alexanderjohnives@gmail.com>
2021-04-19 16:12:38 -0400

    Added content to JS, currently not displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ let wingInformation = {
   wingImage0: {
     id: 'wingInfo1',
     title: 'Ilse WL 1',
-    description1: 'The Child of Light is in the first cave to the left.',
+    description1:
+      'Enter the first cave and walk to the end. Fly or jump down to the stranded boat below.',
     description2:
-      'You need to have found at least one Ancestor Spirit in Daylight Prairie to enter the cave.',
+      'Go near the glowing child and click the "hand with star" symbol to receive a wing for your cape.',
     youtubeLink: 'https://www.youtube.com/embed/_n7BPwsAshE',
   },
   wingImage1: {
@@ -43,6 +44,182 @@ let wingInformation = {
       'ROUTE 2: Flying solo: Fly up to the upper level using the clouds to keep you charged. Once up reach the Child of Light through the tunnel.',
     youtubeLink: 'https://www.youtube.com/embed/yXrNvBOmHM8',
   },
+  wingImage5: {
+    id: 'wingInfo6',
+    title: 'Prairie WL 1 Butterfly Fields',
+    description1: 'The Child of Light is in the first cave to the left.',
+    description2:
+      'You need to have found at least one Ancestor Spirit in Daylight Prairie to enter the cave.',
+    youtubeLink: 'https://youtu.be/GK3hOtc8upo',
+  },
+  wingImage6: {
+    id: 'wingInfo7',
+    title: 'Prairie WL 2 Butterfly Fields',
+    description1:
+      'The Child of Light is on top of the round cave rock. The easiest way to get there is to go inside the the cave and release a call.',
+    description2: 'Butterflies will come and carry you up.',
+    youtubeLink: 'https://youtu.be/6lNBP0oDbxc',
+  },
+  wingImage7: {
+    id: 'wingInfo8',
+    title: 'Prairie WL 3 Butterfly Fields',
+    description1:
+      'The Child of Light is standing by the entrance to the next area (Villages), which is straight ahead of the cloud tunnel.',
+    description2: '',
+    youtubeLink: 'https://youtu.be/5_JFIBKg1oo',
+  },
+  wingImage8: {
+    id: 'wingInfo9',
+    title: 'Prairie WL 4 Villages',
+    description1:
+      'A Child of Light stands in the center of the smaller front island in the middle',
+    description2: '',
+    youtubeLink: 'https://youtu.be/2jotckYmDmI',
+  },
+  wingImage9: {
+    id: 'wingInfo10',
+    title: 'Prairie WL 5 Villages',
+    description1:
+      'Fly to the island to the left. Walk around to the right at the bottom of the hill to find a two player door.',
+    description2:
+      'Both players need to hold a candle to the door to open it and reveal the Child of Light in the cave behind it.',
+    youtubeLink: 'https://youtu.be/ZX6D51_Wais',
+  },
+  wingImage10: {
+    id: 'wingInfo11 Villages',
+    title: 'Prairie WL 6',
+    description1:
+      'Fly to the middle of the three bigger islands and walk around the back of the hill to find a Child of Light.',
+    description2: '',
+    youtubeLink: '',
+  },
+  wingImage11: {
+    id: 'wingInfo12 Villages Temple',
+    title: 'Prairie WL 6',
+    description1:
+      'When you have reached the main temple in the clouds (flying or riding a manta), fly onto the roof just above the door.',
+    description2: 'The Child of Light is to the left.',
+    youtubeLink: 'https://youtu.be/ICQiPq6Si74',
+  },
+  wingImage12: {
+    id: 'wingInfo13 Villages Temple',
+    title: 'Prairie WL 7',
+    description1:
+      'Fly higher up the roof, there is another Child of Light on the dome.',
+    description2: 'Jump into the clouds to recharge wing power.',
+    youtubeLink: 'https://youtu.be/KrtyiKwWxBM',
+  },
+  wingImage13: {
+    id: 'wingInfo14',
+    title: 'Prairie WL 8 Caves',
+    description1:
+      'Coming from Butterfly Fields you first get to a cave with a split lake in the middle. Go up the grassy stairs on the right until you come to a room full of jars.',
+    description2:
+      'Look slightly to the right out of the window and you can see the Child of Light standing in a cave high up in the wall. Fly over, ideally holding the hand of a friend.',
+    youtubeLink: 'https://youtu.be/xg6qtCPCqKc',
+  },
+  wingImage14: {
+    id: 'wingInfo15',
+    title: 'Prairie WL 9 Caves',
+    description1:
+      'Opposite the entrance coming from Butterfly Fields, across the lake there is a narrower cave area that has light shining from the top.',
+    description2:
+      'The Child of Light is standing in a small cave high up to the right.',
+    youtubeLink: 'https://youtu.be/YNF4Vf9KO9w',
+  },
+  wingImage15: {
+    id: 'wingInfo16',
+    title: 'Prairie WL 10 Bird Nest',
+    description1:
+      'This Winged Light is located on a higher island on the perimeter of the area, between the Villages gateway and the Sanctuary Islands entrance.',
+    description2: '',
+    youtubeLink: 'https://youtu.be/9H6hjJI8XGE',
+  },
+  wingImage16: {
+    id: 'wingInfo17',
+    title: 'Prairie WL 11 Bird Nest',
+    description1:
+      'This Winged Light is on a small island directly below the gateway between Bird Nest and Villages.',
+    description2: '',
+    youtubeLink: 'https://youtu.be/7E-4TK_-Wh0',
+  },
+  wingImage13: {
+    id: 'wingInfo18',
+    title: 'Prairie WL 12 Sanctuary islands',
+    description1:
+      'This is visible after you enter the Sanctuary islands area. You have to fly down and then will see the light glistening.',
+    description2:
+      'It is located on the central floating island with a waterfall.',
+    youtubeLink: '',
+  },
+  wingImage14: {
+    id: 'wingInfo19',
+    title: 'Prairie WL 13 Sanctuary islands',
+    description1:
+      'Hidden near the apex of the central island, this winged light is also visible as you fly down.',
+    description2: '',
+    youtubeLink: 'https://youtu.be/N4pVEyYKnks',
+  },
+  wingImage15: {
+    id: 'wingInfo20',
+    title: 'Prairie WL 14 Sanctuary islands',
+    description1:
+      'This Winged light is easily visible when visiting the island’s ancestor spirit.',
+    description2: 'Look over the adjacent cliff face and you will see it.',
+    youtubeLink: 'https://youtu.be/X_KxYDlqcYA',
+  },
+  wingImage16: {
+    id: 'wingInfo21',
+    title: 'Prairie WL 15 Sanctuary islands',
+    description1:
+      'Stand next to the ancestor spirit facing the same direction and fly down off the cliff edge to the cave below.',
+    description2:
+      'At the far side you will find the winged light at the back of the cave.',
+    youtubeLink: 'https://youtu.be/Z7-XS4VA4QM',
+  },
+  wingImage17: {
+    id: 'wingInfo21',
+    title: 'Prairie WL 16 Sanctuary islands',
+    description1:
+      'Follow the waterfall down on the main island from the highest pool.',
+    description2: 'This light is hidden behind the flow on the lowest level.',
+    youtubeLink: 'https://youtu.be/Pq6XytIrbZQ',
+  },
+  wingImage18: {
+    id: 'wingInfo22',
+    title: 'Prairie WL 17 Sanctuary islands',
+    description1:
+      'Facing away from the waterfall at its base, go left and enter the cave.',
+    description2:
+      'Follow a winding path upwards and being careful of the crabby crabs, you will find the winged light.',
+    youtubeLink: 'https://youtu.be/jUZ8xmeqatk',
+  },
+  wingImage19: {
+    id: 'wingInfo23',
+    title: 'Prairie WL 18 Sanctuary islands',
+    description1:
+      'Returning to face the waterfall, there is a cave with ladder structure to the left.',
+    description2: 'The winged light is on the highest horizontal ladder.',
+    youtubeLink: 'https://youtu.be/jAQk4EWryao',
+  },
+  wingImage20: {
+    id: 'wingInfo24',
+    title: 'Prairie WL 19 Sanctuary islands',
+    description1:
+      'This winged light is on one of the furthest satellite floating islands. You will need a friend or some height to reach it.',
+    description2:
+      'You can also use the clouds as you fly down from the area entrance.',
+    youtubeLink: 'https://youtu.be/JGWi8I-FXbw',
+  },
+  wingImage21: {
+    id: 'wingInfo25',
+    title: 'Prairie WL 20 Eight Player Area',
+    description1:
+      'The Child of Light here is in a visible cave at the left-hand tree.',
+    description2:
+      'Fly up the trunk and the Child of Light will be in front of you.',
+    youtubeLink: 'https://youtu.be/Pq6XytIrbZQ',
+  },
 };
 
 /*
@@ -58,7 +235,7 @@ function getCardHTML(wingTitle) {
         <p class="mt-3">${wings['description2']}</p>
     </div>
     <div class="col-sm">
-        <iframe src="${wings['youtubeLink']}" title="YouTube video player"
+        <iframe width="560" height="315" src="${wings['youtubeLink']}" title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen></iframe>
