9cf052e8855c865abea874a43486f4ffcc5c580d
alexjohnives <alexanderjohnives@gmail.com>
2021-04-20 12:23:41 -0400

    Added load event and changed code order

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,21 +1,24 @@
 const btn = document.getElementById('button');
+const serviceID = 'default_service';
+const templateID = 'skyguide contact';
 
-document.getElementById('form').addEventListener('submit', function (event) {
-  event.preventDefault();
+window.addEventListener('load', function () {
+    document
+        .getElementById('form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
 
-  btn.value = 'Sending...';
+            btn.value = 'Sending...';
 
-  const serviceID = 'default_service';
-  const templateID = 'skyguide contact';
-
-  emailjs.sendForm(serviceID, templateID, this).then(
-    () => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    },
-    (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    }
-  );
+            emailjs.sendForm(serviceID, templateID, this).then(
+                () => {
+                    btn.value = 'Send Email';
+                    alert('Sent!');
+                },
+                (err) => {
+                    btn.value = 'Send Email';
+                    alert(JSON.stringify(err));
+                }
+            );
+        });
 });
